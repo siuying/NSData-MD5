41a8caf2ced0c814cc6cb844687b6f5d0c6fd9ef
Francis Chong <francis@ignition.hk>
2012-06-05 13:59:51 +0800

    init commit

---
+++ NSData+MD5Digest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSData+MD5Digest
+//
+//  Created by Francis Chong on 12年6月5日.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSData+MD5Digest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSData+MD5Digest
+//
+//  Created by Francis Chong on 12年6月5日.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
